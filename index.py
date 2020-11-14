10685e3284aa11ca9f4e25e390ea4973376e4be9
Smoqu <cedric.mas.83@gmail.com>
2020-11-14 14:44:08 +0100

    Commit success

--- index.py
+++ index.py
@@ -1,6 +1,7 @@
 from to_bin import to_bin as tb
 from add_zero import add_zero as az
 from reverse_bin import reverse as rvs
+from two_complement import two_complement
 
 number = int(input("Quel est l'entier dont vous voulez faire le complément à 2 ? "))
 how_many_bits = int(input("Sur combien de bits souhaitez-vous encoder votre nombre entier ? "))
@@ -14,5 +15,5 @@ elif number >= 0:
     print(f"Le complément à 2 de l'entier {number} sur {how_many_bits} bits est {result}")
 else:
     abs_binary = tb(abs(number))
-    result = rvs(az(abs_binary, how_many_bits - len(abs_binary)))
+    result = two_complement(rvs(az(abs_binary, how_many_bits - len(abs_binary))))
     print(f"Le complément à 2 de l'entier {number} sur {how_many_bits} bits est {result} ")
